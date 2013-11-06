10bc764f0db66cc3eede1f0e4647284b5fd9a896
rchandanala <rchandanala@zynga.com>
2013-11-06 15:23:01 -0800

    Integrated swipeListener Changes

---
+++ src/com/example/grocerycheckout/interfaces/GroceryCartUpdateListener.java
@@ -0,0 +1,14 @@
+package com.example.grocerycheckout.interfaces;
+
+import java.io.Serializable;
+
+import com.example.grocerycheckout.models.CartItem;
+
+public abstract class GroceryCartUpdateListener implements Serializable {	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	abstract public void onCartItemDelete(CartItem ci);
+	abstract public void onCartItemEdit(CartItem ci);
+}
